7e8799b31624b2c970a708f46ad676e2846ce74c
NinoosMoshi <nalkaton@gmail.com>
2023-11-02 16:21:29 -0500

    add okta

--- frontEnd/shop-ecommerce-ui/src/app/app.module.ts
+++ frontEnd/shop-ecommerce-ui/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontEnd/shop-ecommerce-ui/src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+
+    oidc: {
+        clientId: '0oad2yrvd0tRkMsNu5d7',
+        issuer: 'https://dev-97343831.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+
+}
